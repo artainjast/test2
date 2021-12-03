2a1de0cb046ff2ca5db18fa1f548474de1ee4188
arta Alijan <h.alijani1379@yahoo.com>
2021-12-03 23:43:49 +0330

    FirstCommit

---
+++ src/components/Skills.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <form @submit.prevent="addToSkills">
+   <input type="text" placeholder="Enter your Skill" v-model="skill"/>
+  </form>
+    <ul>
+      <li v-for="item in newskills" :key="item" >{{item}}</li>
+      
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return{
+      skill : '',
+      newskills : [
+        "reactjs" ,
+      ],
+     
+      
+    }
+  },
+  methods: {
+    addToSkills(){
+      this.newskills.push(this.skill)
+      this.skill = ""
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+</style>
